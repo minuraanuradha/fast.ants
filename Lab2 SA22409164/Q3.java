fc9ec17c68a5dc5b796b76e4bba662cf5ed01e00
Minura Anuradha <112975973+minuraanuradha@users.noreply.github.com>
2023-07-05 12:49:18 +0530

    update

--- Lab2 SA22409164/Q3.java
+++
@@ -1,8 +0,0 @@
-class Q3
-{
-    public static void main(String[] args)
-    {
-        System.out.println("You are " + args[0] + " years old"); // Enter your ages
-        System.out.println("You are " + args[1] + " years old");
-    }
-}
