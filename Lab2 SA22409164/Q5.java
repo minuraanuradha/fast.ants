7080f734f3debcf15f9c356254ed4ed7ccaf4313
G.L.S.D.De Silva <136220907+DShey@users.noreply.github.com>
2023-07-05 12:42:26 +0530

    Add files via upload

---
+++ Lab2 SA22409164/Q5.java
@@ -0,0 +1,15 @@
+class Q5 {
+    public static void main(String[] args)
+    {
+     double height = Double.parseDouble(args[0]);
+     double weight = Double.parseDouble(args[1]);
+     double width = Double.parseDouble(args[2]);
+
+     double Volume = height * weight * width ;
+     
+   System.out.println("Volume is  "+Volume);
+     
+     
+    }
+
+}
